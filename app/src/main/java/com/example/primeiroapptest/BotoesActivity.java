2146f63e393be3ebc4ec036fbc7752645d73ab5e
JoaoVieceli <joao.vieceli@universo.univates.br>
2021-03-25 21:11:57 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/example/primeiroapptest/BotoesActivity.java
@@ -0,0 +1,14 @@
+package com.example.primeiroapptest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BotoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_botoes);
+    }
+}
---
+++ app/src/main/java/com/example/primeiroapptest/LoginActivity.java
@@ -0,0 +1,64 @@
+package com.example.primeiroapptest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class LoginActivity extends AppCompatActivity {
+    
+    Button btnLogin;
+    EditText txtUser, txtSenha;
+
+    Context context;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        context = LoginActivity.this;
+
+        btnLogin = findViewById(R.id.btnLogar);
+        txtUser = findViewById(R.id.lblUser);
+        txtSenha = findViewById(R.id.lblSenha);
+
+
+
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String user = txtUser.getText().toString();
+                String password = txtSenha.getText().toString();
+
+                if(user.equals("Joao") && password.equals("123")){
+                    Intent tela = new Intent(context, MenuActivity.class);
+                    startActivity(tela);
+                    finish();
+
+                }else{
+                    exibirToast("Usuário ou Senha incorretos");
+                }
+
+            }
+        });
+    }
+    private void exibirToast(String texto){
+        try{
+            Toast.makeText(context, texto, Toast.LENGTH_LONG).show();
+
+        }catch (Exception ex){
+
+        }
+    }
+
+}
+
