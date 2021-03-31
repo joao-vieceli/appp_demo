2732e4a1cc6d65bb27c4643d76127e66bdbf38f7
JoaoVieceli <joao.vieceli@universo.univates.br>
2021-03-31 11:10:40 -0300

    Terceiro Commit!

--- app/src/main/java/com/example/primeiroapptest/LoginActivity.java
+++ app/src/main/java/com/example/primeiroapptest/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -23,6 +24,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        SharedPreferences sharedPreferences =
+                getSharedPreferences(getString(R.string.pref_key), context.MODE_PRIVATE);
 
         context = LoginActivity.this;
 
@@ -35,21 +38,52 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                String user = txtUser.getText().toString();
-                String password = txtSenha.getText().toString();
+                try{
 
-                if(user.equals("admin") && password.equals("admin")){
-                    Intent tela = new Intent(context, MenuActivity.class);
-                    startActivity(tela);
-                    finish();
 
-                }else{
+                    String user = txtUser.getText().toString();
+                    String password = txtSenha.getText().toString();
 
-                    exibirToast("Usuário ou Senha incorretos");
+                    if(user.equals("admin") && password.equals("admin")){
+
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("usuario" , txtUser.getText().toString());
+
+                        if(editor.commit()){
+
+                            Intent tela = new Intent(context, MenucActivity.class);
+                            startActivity(tela);
+                            finish();
+
+
+                        }else{
+
+                            exibirToast("Aconteceu alguem erro!");
+
+                        }
+
+                    }else{
+                        exibirToast("Usuário ou Senha incorretos!");
+                    }
+
+
+                }catch(Exception ex){
+
+                    exibirToast(ex.getMessage());
+
                 }
 
             }
         });
+
+        String nome_usuario = sharedPreferences.getString("usuario","");
+        if (!nome_usuario.equals("")) {
+
+            Intent tela = new Intent(context, MenucActivity.class);
+            startActivity(tela);
+
+        }
+
     }
     private void exibirToast(String texto){
         try{
